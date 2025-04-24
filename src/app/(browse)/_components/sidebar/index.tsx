7a2f3013cffd3cf4b2f951fc8b455f50b943a616
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2025-04-24 15:55:49 -0300

    feat(sidebar): add sidebar including toggle option

---
+++ src/app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,13 @@
+import { Toggle } from '@/app/(browse)/_components/sidebar/toggle';
+import { Wrapper } from './wrapper';
+
+export const Sidebar = () => {
+  // fetch followed
+  // fetch recommended
+
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
+};
